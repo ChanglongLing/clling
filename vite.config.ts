54821aa2c0514ab69ecdaef01d9a7720b0a37ae6
ChanglongLing <clling206@gmail.com>
2025-08-28 00:05:17 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/clling.github.io/',   // 👈 注意这里，项目页必须写仓库名
+  base: "/clling/",
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
+
